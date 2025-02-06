ade1cbc0f0d7b8da6c5da45ca56e8592375b7c28
mamidisuryakumari <kumari.mudila@gmail.com>
2025-02-06 11:26:04 +0530

    commit the changes

--- src/test/java/com/ecommerce/runners/TestRunner.java
+++ src/test/java/com/ecommerce/runners/TestRunner.java
@@ -8,13 +8,12 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/com/ecommrece/features", 
                  glue = {"com.ecommerce.stepDefinitions", "com.ecommerce.hooks"},
-                		 tags = "@happy-path1",
+                		 tags = "@TestCICD",
                  plugin = {"pretty", "html:target/cucumber-reports.html",
                 		 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
                  dryRun = false,
                  monochrome = true
 )
-
 
 
 
